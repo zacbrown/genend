9d26feddadc5fa512ea9463962643abdd7bfd69f
Zac Brown <zac@zacbrown.org>
2009-03-09 00:41:08 -0400

    Change graphing to include all k-mers for particular piece size.

--- scripts/process_scores.rb
+++ scripts/process_scores.rb
@@ -59,6 +59,8 @@ File.open(file, 'r').each { |line|
   score_vals[tokens[0]] << Record.new(tokens[1] == tokens[2], tokens[3].to_f)
 }
 
+graph_hash = {}
+
 score_vals.each { |key,value|
   next if value == []
   value.sort! { |a,b| a.score <=> b.score }
@@ -71,31 +73,35 @@ score_vals.each { |key,value|
     match_arr << record.match
   }
 
-  graph_arr = process_groups(score_arr, match_arr)
-
-  graph = Gruff::Line.new
-  graph.theme_keynote
-  graph.title = "#{key}-mer #{header} - #{piece_size} bp pieces - 100 draws"
-  graph.data "percent identified", graph_arr
-  graph.x_axis_label = "probability score"
-  graph.y_axis_label = "percent identified"
-
-  # prep labels for x axis
-  low = ("%5.2f" % score_arr[0]).to_f
-  high = ("%5.2f" % score_arr[score_arr.size - 1]).to_f
-  incr = ("%5.2f" % ((high - low) / 5.0)).to_f
-  graph_chunks = graph_arr.size / 5
-  graph_x_vals = (1..4).to_a
-  graph_x_vals.map! { |val| val * graph_chunks }
-
-  cur_score_val = low
-  graph.labels[0] = low.to_s
-  graph.labels[graph_arr.size - 1] = high.to_s
-  graph_x_vals.each { |val|
-    graph.labels[val] = (cur_score_val + incr).to_s
-    cur_score_val += incr
-  }
-
-  graph.write("result-#{header}-#{key}-#{piece_size}-#{date}.png")
+  graph_hash[key] = process_groups(score_arr, match_arr)
 }
+
+graph = Gruff::Line.new
+graph.theme_keynote
+graph.title = "#{header} - #{piece_size} bp pieces - 100 draws"
+graph.x_axis_label = "probability score"
+graph.y_axis_label = "percent identified"
+
+# setup data
+graph_hash.each { |key,value|
+  graph.data "#{key}-mer", value
+}
+
+# prep labels for x axis
+low = ("%5.2f" % graph_hash['8'][0]).to_f
+high = ("%5.2f" % graph_hash['8'][graph_hash['8'].size - 1]).to_f
+incr = ("%5.2f" % ((high - low) / 5.0)).to_f
+graph_chunks = graph_hash['8'].size / 5
+graph_x_vals = (1..4).to_a
+graph_x_vals.map! { |val| val * graph_chunks }
+
+cur_score_val = low
+graph.labels[0] = low.to_s
+graph.labels[graph_hash['8'].size - 1] = high.to_s
+graph_x_vals.each { |val|
+  graph.labels[val] = (cur_score_val + incr).to_s
+  cur_score_val += incr
+}
+
+graph.write("result-#{header}-#{piece_size}-#{date}.png")
 
