1b0b132b377555f47adb3f46685ea47b3c16fee7
Zac Brown <zac@zacbrown.org>
2009-03-25 21:57:43 -0400

    fix file split script.

--- scripts/split_res_match.rb
+++ scripts/split_res_match.rb
@@ -5,15 +5,24 @@ size = file_toks[1]
 file_h = nil
 cur_spec = ""
 
+spec_1_fh = File.open('NC_009613-#{size}-match.dat', 'a')
+spec_2_fh = File.open('NC_004603-#{size}-match.dat', 'a')
+spec_3_fh = File.open('NC_007622-#{size}-match.dat', 'a')
+spec_4_fh = File.open('NC_003295-#{size}-match.dat', 'a')
+spec_5_fh = File.open('NC_005823-#{size}-match.dat', 'a')
+
 File.open(ARGV[0], 'r').each { |line|
   tokens = line.split(' ')
-  if cur_spec != tokens[1]
-    cur_spec = tokens[1]
-    file_h.close
-    file_h = File.open('#{tokens[1]}-#{size}.dat', 'w')
-  end
 
-  file_h.write(line)
+  spec_1_fh.write(line) if tokens[1] == 'NC_009613'
+  spec_2_fh.write(line) if tokens[1] == 'NC_004603.2'
+  spec_3_fh.write(line) if tokens[1] == 'NC_007622'
+  spec_4_fh.write(line) if tokens[1] == 'NC_003295'
+  spec_4_fh.write(line) if tokens[1] == 'NC_005823.2'
 }
 
-file_h.close
+spec_1_fh.close
+spec_2_fh.close
+spec_3_fh.close
+spec_4_fh.close
+spec_5_fh.close
