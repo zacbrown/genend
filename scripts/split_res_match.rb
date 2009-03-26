e66716f2199e6dfcf205a1543fe473cc5d014220
Zac Brown <zac@zacbrown.org>
2009-03-25 22:36:00 -0400

    fix minor error in file split script

--- scripts/split_res_match.rb
+++ scripts/split_res_match.rb
@@ -1,25 +1,19 @@
 #!/usr/bin/env ruby
-
-file_toks = ARGV[0].split('-')
-size = file_toks[1].to_i
-puts file_toks[1].class
-file_h = nil
-cur_spec = ""
-
-spec_1_fh = File.open('NC_009613-#{size}-match.dat', 'a')
-spec_2_fh = File.open('NC_004603-#{size}-match.dat', 'a')
-spec_3_fh = File.open('NC_007622-#{size}-match.dat', 'a')
-spec_4_fh = File.open('NC_003295-#{size}-match.dat', 'a')
-spec_5_fh = File.open('NC_005823-#{size}-match.dat', 'a')
+spec_1_fh = File.open('NC_009613-10000-match.dat', 'a')
+spec_2_fh = File.open('NC_004603-10000-match.dat', 'a')
+spec_3_fh = File.open('NC_007622-10000-match.dat', 'a')
+spec_4_fh = File.open('NC_003295-10000-match.dat', 'a')
+spec_5_fh = File.open('NC_005823-10000-match.dat', 'a')
 
 File.open(ARGV[0], 'r').each { |line|
   tokens = line.split(' ')
 
+  puts tokens[1]
   spec_1_fh.write(line) if tokens[1] == 'NC_009613'
   spec_2_fh.write(line) if tokens[1] == 'NC_004603.2'
   spec_3_fh.write(line) if tokens[1] == 'NC_007622'
   spec_4_fh.write(line) if tokens[1] == 'NC_003295'
-  spec_4_fh.write(line) if tokens[1] == 'NC_005823.2'
+  spec_5_fh.write(line) if tokens[1] == 'NC_005823.2'
 }
 
 spec_1_fh.close
