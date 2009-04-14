e25ec5b680fee857a98557f81b50ffe218938859
Zac Brown <zac@zacbrown.org>
2009-04-14 15:29:20 -0400

    Various changes for process_scores.rb and remove deleted files.

--- scripts/process_scores.rb
+++ scripts/process_scores.rb
@@ -1,7 +1,10 @@
-#!/usr/bin/env ruby
+##!/usr/bin/env ruby
 
 require 'rubygems'
+gem PLATFORM == 'java' ? 'rmagick4j' : 'rmagick'
+require 'RMagick'
 require 'gruff'
+require 'benchmark'
 #require 'gsl'
 
 SPACINGS = 50
@@ -35,15 +38,15 @@ def process_groups(score_array, match_array)
 
   while cur_chunk_ind < len
     if cur_chunk_ind + chunk_size > len
-      tmp_score_array = score_array[cur_chunk_ind, len - 1]
-      tmp_match_array = match_array[cur_chunk_ind, len - 1]
+      tmp_score_array = score_array[cur_chunk_ind..(len-1)]
+      tmp_match_array = match_array[cur_chunk_ind..(len-1)]
     else
-      tmp_score_array = score_array[cur_chunk_ind, chunk_size - 1]
-      tmp_match_array = match_array[cur_chunk_ind, chunk_size - 1]
+      tmp_score_array = score_array[cur_chunk_ind, chunk_size]
+      tmp_match_array = match_array[cur_chunk_ind, chunk_size]
     end
 
     count = tmp_match_array.count_true
-    perc = ("%5.4f" % (count.to_f / chunk_size.to_f * 100)).to_f
+    perc = ("%5.4f" % (count.to_f / tmp_match_array.size.to_f * 100)).to_f
     tmp_array = Array.new
 
     for i in 0 .. (tmp_score_array[-1] - tmp_score_array[0]).to_i
@@ -77,71 +80,79 @@ header = file.split('-')[0]
 NUM_KMERS = 31050
 
 score_vals = {'3'=>[],'4'=>[],'5'=>[],'6'=>[],'7'=>[],'8'=>[]}
+puts "processing: #{file}"
 
-File.open(file, 'r').each { |line|
-  tokens = line.split(' ')
-  score_vals[tokens[0]] << Record.new(tokens[1] == tokens[2], tokens[3].to_f)
-}
+Benchmark.bm do |make|
 
-graph_hash = {}
-low = 0.0; high = 0.0;
-short_len = 1000000000
+  make.report do
+    File.open(file, 'r').each { |line|
+      tokens = line.split(' ')
+      score_vals[tokens[0]] << Record.new(tokens[1] == tokens[2], tokens[3].to_f)
+    }
 
-score_vals.each { |key,value|
-  next if value == []
-  value.sort! { |a,b| a.score <=> b.score }
-  puts "processing: #{key}-mers"
+    graph_hash = {}
+    low = 0.0; high = 0.0;
+    short_len = 1000000000
 
-  score_arr = Array.new
-  match_arr = Array.new
-  value.each { |record|
-    score_arr << record.score
-    match_arr << record.match
-  }
+    score_vals.each { |key,value|
+      next if value == []
+      value.sort! { |a,b| a.score <=> b.score }
+      puts "processing: #{key}-mers | num elements: #{value.length}"
 
-  short_len = score_arr.size if score_arr.size < short_len
-  low = ("%5.2f" % score_arr[0]).to_f if key == '3'
-  high = ("%5.2f" % score_arr[score_arr.size - 2]).to_f if key == '8'
-  graph_hash[key] = process_groups(score_arr, match_arr)
-}
+      score_arr = Array.new
+      match_arr = Array.new
+      value.each { |record|
+        score_arr << record.score
+        match_arr << record.match
+      }
 
-# truncate arrays that are longer than the shortest in the group
-graph_hash.each { |key,value|
-  if value.size > short_len
-    diff = value.size - short_len
-    value.slice! 0, diff
+      short_len = score_arr.size if score_arr.size < short_len
+      low = ("%5.5f" % score_arr[0]).to_f if key == '3'
+      high = ("%5.5f" % score_arr[score_arr.size - 2]).to_f if key == '8'
+      graph_hash[key] = process_groups(score_arr, match_arr)
+    }
+
+    # truncate arrays that are longer than the shortest in the group
+    graph_hash.each { |key,value|
+      if value.size > short_len
+        diff = value.size - short_len
+        value.slice! 0, diff
+      end
+    }
+
+    graph = Gruff::Line.new
+    graph.theme_keynote #2E37FE
+    graph.replace_colors ["#ff0000", "#00ff00", "#3333FF",
+                          "#ff00ff", "#ffff00", "#ffffff"]
+    graph.title = "#{header} - #{piece_size} bp pieces - 100 draws"
+    graph.x_axis_label = "probability score"
+    graph.y_axis_label = "percent identified"
+    graph.marker_font_size = 14
+    graph.maximum_value = 100
+    graph.minimum_value = 0
+
+    # setup data
+    graph_hash.each { |key,value|
+      graph.data "#{key}-mer", value
+    }
+
+    # prep labels for x axis
+    low /= piece_size.to_f; high /= piece_size.to_f
+    incr = ("%5.3f" % ((high - low) / 5.0)).to_f
+    puts "low: #{low} || high: #{high} || incr: #{incr}"
+    graph_chunks = graph_hash['8'].size / 5
+    graph_x_vals = (1..4).to_a
+    graph_x_vals.map! { |val| val * graph_chunks }
+
+    cur_score_val = low
+    graph.labels[0] = low.to_s
+    graph.labels[graph_hash['8'].size - 1] = high.to_s
+    graph_x_vals.each { |val|
+      graph.labels[val] = ("%3.4f" % (cur_score_val + incr))
+      cur_score_val += incr
+    }
+
+    graph.write("result-#{header}-#{piece_size}-#{date}.png")
+
   end
-}
-
-graph = Gruff::Line.new
-graph.theme_keynote #2E37FE
-graph.replace_colors ["#ff0000", "#00ff00", "#3333FF",
-                      "#ff00ff", "#ffff00", "#ffffff"]
-graph.title = "#{header} - #{piece_size} bp pieces - 100 draws"
-graph.x_axis_label = "probability score"
-graph.y_axis_label = "percent identified"
-graph.maximum_value = 100
-graph.minimum_value = 0
-
-# setup data
-graph_hash.each { |key,value|
-  graph.data "#{key}-mer", value
-}
-
-# prep labels for x axis
-incr = ("%5.2f" % ((high - low) / 5.0)).to_f
-puts "low: #{low} || high: #{high}"
-graph_chunks = graph_hash['6'].size / 5
-graph_x_vals = (1..4).to_a
-graph_x_vals.map! { |val| val * graph_chunks }
-
-cur_score_val = low
-graph.labels[0] = low.to_s
-graph.labels[graph_hash['8'].size - 1] = high.to_s
-graph_x_vals.each { |val|
-  graph.labels[val] = (cur_score_val + incr).to_s
-  cur_score_val += incr
-}
-
-graph.write("result-#{header}-#{piece_size}-#{date}.png")
-
+end
